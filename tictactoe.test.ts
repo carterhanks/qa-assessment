de7a6c2ab9eeb25cc01c5b68f864b572e490ddb4
carterhanks <carterhanks@gmail.com>
2021-06-25 10:51:24 -0600

    3 Tests Added, Didn't Finish Testing for O

--- tictactoe.test.ts
+++ tictactoe.test.ts
@@ -16,5 +16,26 @@ test('I can start a game', async () => {
 
     let button = await (await driver).findElement(By.id('start-game'));
     await button.click();
+    await driver.sleep(2000);
     
 });
+
+test('upper left square adds x on click', async () => {
+    await driver.findElement(By.id('cell-0')).click();
+    await driver.sleep(2000);
+});
+
+test('upper right square adds x on click', async () => {
+    await driver.findElement(By.id('cell-2')).click();
+    await driver.sleep(2000);
+});
+
+test('bottom right square adds x on click', async () => {
+    await driver.findElement(By.id('cell-8')).click();
+    await driver.sleep(2000);
+});
+
+// test('computer adds O', async () => {
+//     await driver.findElement(By.xpath('(//*[text()="cell-1"])'));
+//     expect(cell([1]).toEqual('O'));
+// });
